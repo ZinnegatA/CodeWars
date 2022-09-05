4c795bbb0bb2830ec3fc080f1e7fb1ebd97168ae
Ruslan <ruslanibr123@gmail.com>
2022-09-05 11:36:05 +0600

    Added CodeWars #2 file

---
+++ CodeWars #2.py
@@ -0,0 +1,12 @@
+# 1
+# def correct(s):
+#     res = s.replace('5', 'S').replace('0', 'O').replace('1', 'I')
+#     return res
+#
+#
+# print(correct('L0ND0N'))
+
+# 2
+def sum_dig_pow(a, b):
+    res = []
+
